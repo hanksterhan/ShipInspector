2af60821da24feb87a9e169ab2361be9a38c6e4c
hanksterhan <henryhan62@gmail.com>
2025-12-13 17:25:34 -0800

    Creating the first few interfaces for defining and parsing poker hands

---
+++ common/src/interfaces/handInterfaces.ts
@@ -0,0 +1,75 @@
+/**
+ * Card rank values (2-14)
+ * 2-10: numbered cards
+ * 11: Jack
+ * 12: Queen
+ * 13: King
+ * 14: Ace
+ */
+export type CardRank = 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
+
+/**
+ * Card suit values
+ */
+export type CardSuit = "c" | "d" | "h" | "s";
+
+export interface Card {
+    rank: CardRank;
+    suit: CardSuit;
+}
+
+/**
+ * A hole represents a player's private cards (exactly 2 cards in Texas Hold'em)
+ */
+export interface Hole {
+    cards: [Card, Card];
+}
+
+/**
+ * A deck of 52 cards
+ */
+export interface Deck {
+    cards: Card[];
+}
+
+/**
+ * The board (community cards) can have 0-5 cards
+ * - Pre-flop: 0 cards
+ * - Flop: 3 cards
+ * - Turn: 4 cards
+ * - River: 5 cards
+ */
+export interface Board {
+    cards: Card[];
+}
+
+/**
+ * Parse a card string into a Card object
+ * @param card - The card string to parse
+ * @returns The Card object
+ */
+export function parseCard(card: string): Card {
+    const rank = card.slice(0, -1);
+    const suit = card.slice(-1);
+    return { rank: parseInt(rank) as CardRank, suit: suit as CardSuit };
+}
+
+/**
+ * Parse a hole string into a Hole object
+ * @param hole - The hole string to parse "Ah 2c" (Ace of Hearts and 2 of Clubs)
+ * @returns The Hole object
+ */
+export function parseHole(hole: string): Hole {
+    const cards = hole.split(" ");
+    return { cards: [parseCard(cards[0]), parseCard(cards[1])] };
+}
+
+/**
+ * Parse a board string into a Board object
+ * @param board - The board string to parse "Kd 9h 2c" (King of Diamonds, 9 of Hearts, and 2 of Clubs)
+ * @returns The Board object
+ */
+export function parseBoard(board: string): Board {
+    const cards = board.split(" ");
+    return { cards: cards.map(parseCard) };
+}
--- common/src/interfaces/index.ts
+++ common/src/interfaces/index.ts
@@ -1,5 +1,1 @@
-export * from "./userInfoInterfaces";
-export * from "./rosterInfoInterfaces";
-export * from "./leagueInfoInterfaces";
-export * from "./leageStandingsInterfaces";
-export * from "./sleeperInterfaces";
+export * from "./handInterfaces";
