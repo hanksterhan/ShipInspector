367fd425582c0afc44be02e68d2f052fdd0a90f9
hanksterhan <henryhan62@gmail.com>
2025-12-15 15:45:48 -0800

    boilerplate for the Deck Store

--- web/src/stores/index.ts
+++ web/src/stores/index.ts
@@ -1,8 +1,11 @@
 import { MenuStore } from "./MenuStore";
 import { CardStore } from "./CardStore";
+import { DeckStore } from "./DeckStore";
 // PLOP: APPEND STORE IMPORTS
 
 export const menuStore = new MenuStore();
 export * from "./CardStore";
 export const cardStore = new CardStore();
+export * from "./DeckStore";
+export const deckStore = new DeckStore();
 // PLOP: APPEND STORE EXPORTS
