6c09f7fcb6650300d35400ede20f0a281ca32083
hanksterhan <henryhan62@gmail.com>
2025-12-15 13:16:11 -0800

    added the 4 suits to icons

--- web/src/assets/index.ts
+++ web/src/assets/index.ts
@@ -1,2 +1,7 @@
 export * from "./poker.svg";
 export * from "./percentage.svg";
+export * from "./hearts.svg";
+export * from "./clubs.svg";
+export * from "./diamonds.svg";
+export * from "./spades.svg";
+// PLOP: APPEND ICON EXPORTS
