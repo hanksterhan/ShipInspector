19ed59455b9fbee0cfbd6cf026df3e87f820a7e2
hanksterhan <henryhan62@gmail.com>
2025-12-15 13:19:05 -0800

    boilerplate for card store

--- web/src/stores/index.ts
+++ web/src/stores/index.ts
@@ -1,5 +1,8 @@
 import { MenuStore } from "./MenuStore";
+import { CardStore } from "./CardStore"
 // PLOP: APPEND STORE IMPORTS
 
 export const menuStore = new MenuStore();
-// PLOP: APPEND STORE EXPORTS
+export * from "./CardStore";
+export const cardStore = new CardStore();
+// PLOP: APPEND STORE EXPORTS 
