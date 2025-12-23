6a227b8677d631a8e58c27adffe700149703ecfc
hanksterhan <henryhan62@gmail.com>
2025-12-22 23:01:30 -0800

    adding a compass icon

---
+++ admin/src/assets/compass.svg.ts
@@ -0,0 +1,7 @@
+import { svg } from "lit";
+
+export const compassIcon = svg`
+<svg>
+
+</svg>
+`;
--- admin/src/assets/index.ts
+++ admin/src/assets/index.ts
@@ -1,3 +1,4 @@
 export * from "./visible.svg";
 export * from "./invisible.svg";
+export * from "./compass.svg";
 // PLOP: APPEND ICON EXPORTS
