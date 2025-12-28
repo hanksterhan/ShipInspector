3f1bfca12854bf8367eb70f661c6e0d8767f9be7
hanksterhan <henryhan62@gmail.com>
2025-12-28 01:19:12 -0800

    refactoring to import just the components i use instead of all of spectrum

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,19 @@
 import "./pages";
 import "./components";
 
-import "@spectrum-web-components/bundle/elements.js";
+// Import only the Spectrum Web Components we actually use
+import "@spectrum-web-components/theme/sp-theme.js";
+import "@spectrum-web-components/progress-circle/sp-progress-circle.js";
+import "@spectrum-web-components/action-button/sp-action-button.js";
+import "@spectrum-web-components/button/sp-button.js";
+import "@spectrum-web-components/accordion/sp-accordion.js";
+import "@spectrum-web-components/accordion/sp-accordion-item.js";
+import "@spectrum-web-components/slider/sp-slider.js";
+import "@spectrum-web-components/field-label/sp-field-label.js";
+import "@spectrum-web-components/action-group/sp-action-group.js";
+import "@spectrum-web-components/table/sp-table.js";
+import "@spectrum-web-components/table/sp-table-head.js";
+import "@spectrum-web-components/table/sp-table-head-cell.js";
+import "@spectrum-web-components/table/sp-table-body.js";
+import "@spectrum-web-components/table/sp-table-row.js";
+import "@spectrum-web-components/table/sp-table-cell.js";
