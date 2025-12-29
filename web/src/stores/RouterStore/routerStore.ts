16e879cb2b196ae35e6d672988e0700d3543fc74
hanksterhan <henryhan62@gmail.com>
2025-12-28 17:27:34 -0800

    fixing routerstore error

--- web/src/stores/RouterStore/routerStore.ts
+++ web/src/stores/RouterStore/routerStore.ts
@@ -6,6 +6,9 @@ export class RouterStore {
     @observable
     currentRoute: Route = "/";
 
+    // Callback for route changes - will be set by AuthStore
+    private onRouteChange?: (route: Route) => void;
+
     constructor() {
         makeObservable(this);
         this.init();
@@ -21,16 +24,32 @@ export class RouterStore {
         this.setRouteFromPath(window.location.pathname);
     }
 
+    /**
+     * Register a callback to be called when route changes
+     * Used by AuthStore to check authentication on route changes
+     */
+    setRouteChangeListener = (callback: (route: Route) => void): void => {
+        this.onRouteChange = callback;
+    }
+
     @action
     navigate(route: Route): void {
         if (this.currentRoute !== route) {
+            const oldRoute = this.currentRoute;
             this.currentRoute = route;
             window.history.pushState({}, "", route);
+            
+            // Notify listener of route change (for auth checks)
+            if (this.onRouteChange && oldRoute !== route) {
+                this.onRouteChange(route);
+            }
         }
     }
 
     @action
     private setRouteFromPath(path: string): void {
+        const oldRoute = this.currentRoute;
+        
         // Map paths to routes
         if (path === "/" || path === "/signin" || path === "/login") {
             this.currentRoute = "/";
@@ -41,6 +60,11 @@ export class RouterStore {
         } else {
             // Default to root
             this.currentRoute = "/";
+        }
+        
+        // Notify listener of route change (for auth checks)
+        if (this.onRouteChange && oldRoute !== this.currentRoute) {
+            this.onRouteChange(this.currentRoute);
         }
     }
 
