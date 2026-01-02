d9637a18cc415187b151bf2cf366723caa6fa15f
hanksterhan <henryhan62@gmail.com>
2026-01-01 17:17:41 -0800

    moved table logo placement

--- web/src/assets/table.svg.ts
+++ web/src/assets/table.svg.ts
@@ -55,7 +55,7 @@ export const tableIcon = svg`
   <text
     class="board-label"
     x="600"
-    y="200"
+    y="400"
     text-anchor="middle"
     font-size="24"
     fill="rgba(255,255,255,0.4)"
